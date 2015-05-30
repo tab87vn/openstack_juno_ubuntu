dbf32405d6ad1ae9c28e13e4a1508746650fc2d0
Tuan-Anh Bui <tuananh.hufs@gmail.com>
2015-05-30 16:12:27 +0200

    fix config file

--- common.sh
+++ common.sh
@@ -3,55 +3,55 @@
 #  common.sh
 
 export DEBIAN_FRONTEND=noninteractive
-export ETH0_IP=$(ifconfig eth0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
-export ETH1_IP=$(ifconfig eth1 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
-export ETH2_IP=$(ifconfig eth2 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
 
-# LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
-export INSTALL_DIR = $(pwd)
+# LOCAL_PATH=os.path.dirname(os.path.abspath(__file__))
+export INSTALL_DIR=$(pwd)
 
 # dynamic ip retrieval
+ETH0_IP=$(ifconfig eth0 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
+ETH1_IP=$(ifconfig eth1 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
+ETH2_IP=$(ifconfig eth2 | awk '/inet addr/ {split ($2,A,":"); print A[2]}')
 
-export MNG_NET_IP = $ETH0
-export VMN_NET_IP = $ETH1
-export EXT_NET_IP = $ETH2
+export MNG_NET_IP=$ETH0
+export VMN_NET_IP=$ETH1
+export EXT_NET_IP=$ETH2
 
-export ETH2_NETWORK = 192.168.100.0
+export ETH2_NETWORK=192.168.100.0
 
-export CTL_ETH0_IP = 172.31.20.200
-export CTL_ETH1_IP = 172.31.22.200
-export CTL_ETH2_IP = 172.31.23.200
+export CTL_ETH0_IP=172.31.20.200
+export CTL_ETH1_IP=172.31.22.200
+export CTL_ETH2_IP=172.31.23.200
 
-export NET_ETH0_IP = 172.31.20.201
-export NET_ETH1_IP = 172.31.22.201
-export NET_ETH2_IP = 172.21.23.201
+export NET_ETH0_IP=172.31.20.201
+export NET_ETH1_IP=172.31.22.201
+export NET_ETH2_IP=172.21.23.201
 
-export CP1_ETH0_IP = 172.31.20.202
-export CP1_ETH1_IP = 172.31.22.202
-export CP1_ETH2_IP = 172.31.23.202
+export CP1_ETH0_IP=172.31.20.202
+export CP1_ETH1_IP=172.31.22.202
+export CP1_ETH2_IP=172.31.23.202
 
-export CP2_ETH0_IP = 172.31.20.203
-export CP2_ETH1_IP = 172.31.22.203
-export CP2_ETH2_IP = 172.31.23.203
+export CP2_ETH0_IP=172.31.20.203
+export CP2_ETH1_IP=172.31.22.203
+export CP2_ETH2_IP=172.31.23.203
 
 
-export COM_MNG_IF = eth0
-export COM_MNG_BR = br-int
+export COM_MNG_IF=eth0
+export COM_MNG_BR=br-int
 
-export COM_VMN_IF = eth1 #em2
-export COM_VMN_BR = br-eth1
+export COM_VMN_IF=eth1 #em2
+export COM_VMN_BR=br-eth1
 
-export COM_EXT_IF = eth2 #em1
-export COM_EXT_BR = br-ex
+export COM_EXT_IF=eth2 #em1
+export COM_EXT_BR=br-ex
 
-export NET_MNG_IF = eth0
-export NET_MNG_BR = br-int
+export NET_MNG_IF=eth0
+export NET_MNG_BR=br-int
 
-export NET_VMN_IF = eth1 #em2
-export NET_VMN_BR = br-eth1
+export NET_VMN_IF=eth1 #em2
+export NET_VMN_BR=br-eth1
 
-export NET_EXT_IF = eth2 #em1
-export NET_EXT_BR = br-ex
+export NET_EXT_IF=eth2 #em1
+export NET_EXT_BR=br-ex
 
 
 
